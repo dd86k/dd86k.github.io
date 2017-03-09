a2bf8c1814d89a6b15a8dec2a1c65830d8b173d5
dd <devddstuff@gmail.com>
2017-03-09 13:09:59 -0500

    dd-dos + new style

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-function cl()
+"use strict";
+
+onload = s;
+
+var n, l;
+
+function s()
 {
-    var s1 = document.getElementById("s1");
-    var s2 = document.getElementById("s2");
-    var s3 = document.getElementById("s3");
-    var s4 = document.getElementById("s4");
-    
-    var c = s1.style.color.toString();
-    s1.style.color = s4.style.color.toString();
-    s4.style.color = s3.style.color.toString();
-    s3.style.color = s2.style.color.toString();
-    s2.style.color = c;
+    var h = document.getElementById("t");
+    var str = "dd86k";
+    l = str.length;
+    for (var i = 0; i < l; ++i) { // http://stackoverflow.com/a/5365036
+        var s = document.createElement("span");
+        s.style.color = '#' + (Math.random() * (0xFFFFFF) << 0).toString(16);
+        s.innerHTML = str[i];
+        h.appendChild(s);
+    }
+    n = h.children;
+    setInterval(cl, 1000);
 }
 
-setInterval(cl, 500);
+function cl()
+{
+    var c = n[l - 1].style.color;
+    for (var i = l - 2; i >= 0; --i)
+        n[i + 1].style.color = n[i].style.color;
+    n[0].style.color = c;
+}
 
 function e(f)
 {
