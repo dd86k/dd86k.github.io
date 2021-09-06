2d0e5f146ac689fd2e18a884390fc3ed48176d5c
dd86k <dd@dax.moe>
2021-09-06 11:44:49 -0400

    Refresh portfolio

--- oldstuff/pages/checkdem/size64.js
+++
@@ -1,9 +0,0 @@
-/**
- * size64.js by DD~!
- */
-
-function size64(s)
-{
-    var o = s.length.toString(16);
-    return Array(17 - o.length).join("0") + o;
-}
--- oldstuff/pages/eight/button.js
+++
@@ -1,6 +0,0 @@
-﻿"use strict"; //Always, baby
-
-function ee()
-{
-    new Audio("eight/eight.mp3").play();
-}
